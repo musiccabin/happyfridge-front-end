4e5e590747eee043831e425b842e5cfac814688c
EdinK1 <edinkaymakchi@gmail.com>
2020-10-24 13:16:44 -0700

    init expo and folder structure

---
+++ js/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Test = () => {
+  return (
+    <View>
+      <Text>test</Text>
+    </View>
+  )
+}
+
+export default Test
---
+++ js/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import { Test } from '../components'
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Test />
+    </SafeAreaView>
+  )
+}
+
+export default Home
