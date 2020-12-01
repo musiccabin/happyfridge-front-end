aa14df0aa2aceaaea31c8167d0c4174551c68ab6
Rakesh <rakesh22717@gmail.com>
2020-11-30 17:38:51 -0800

    requested changes fixed

--- js/styles/globalStyles.js
+++ js/styles/globalStyles.js
@@ -104,6 +104,16 @@ const globalStyles = StyleSheet.create({
   pinCenter: {
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  absoluteCenterContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
 
