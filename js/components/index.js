4af6b7987e27f1d506a05ce905f240145f30cc40
EdinK1 <edinkaymakchi@gmail.com>
2020-12-17 10:03:34 -0800

    add leftover list component

--- js/components/index.js
+++ js/components/index.js
@@ -3,4 +3,5 @@ import CardList from './CardList'
 import Recipe from './Recipe'
 import Carousel from './Carousel'
 import Button from './Button'
-export { Card, Recipe, Carousel, Button, CardList }
+import LeftOverList from './LeftOverList'
+export { Card, Recipe, Carousel, Button, CardList, LeftOverList }
