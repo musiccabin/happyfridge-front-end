f2dc060c73e29f6a2dbaff4acf147b6f85582da5
EdinK1 <edinkaymakchi@gmail.com>
2020-12-28 12:59:51 -0800

    add menu icons

---
+++ assets/menu-icons/index.js
@@ -0,0 +1,23 @@
+import HomeIcon from './home.svg'
+import HomeInActiveIcon from './home-inactive.svg'
+import GroceryIcon from './grocery.svg'
+import GroceryInActiveIcon from './grocery-inactive.svg'
+import FavoriteIcon from './favorite.svg'
+import FavoriteInActiveIcon from './favorite-inactive.svg'
+import MealPlanIcon from './mealplan.svg'
+import MealPlanInActiveIcon from './mealplan-inactive.svg'
+import LeftOverIcon from './leftover.svg'
+import LeftOverInActiveIcon from './leftover-inactive.svg'
+
+export {
+  HomeIcon,
+  HomeInActiveIcon,
+  GroceryIcon,
+  GroceryInActiveIcon,
+  FavoriteIcon,
+  FavoriteInActiveIcon,
+  MealPlanIcon,
+  MealPlanInActiveIcon,
+  LeftOverIcon,
+  LeftOverInActiveIcon,
+}
