a0852b579f3aa33310376392fcbca2f5d2c045ac
Susannah Sun <susannahsun@Susannahs-MacBook-Pro.local>
2021-01-20 20:52:13 -0800

    groceries query changes

--- js/screens/GroceryList.js
+++ js/screens/GroceryList.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react'
 import { SafeAreaView } from 'react-native'
 import { globalStyles } from '../styles'
 import { GroceryListTabs } from '../navigation'
-import { produceData, meatData, frozenData, dairyData, nutsData } from '../mock'
+import { useQuery } from '@apollo/client'
+import { groceriesQuery } from '../graphql/queries'
+// import { produceData, meatData, frozenData, dairyData, nutsData } from '../mock'
 
 const GroceryList = () => {
   const [structuredData, setStructuredData] = useState()
@@ -11,6 +13,37 @@ const GroceryList = () => {
   if (error) console.error(error)
 
   useEffect(() => {
+    const allData = data.groceriesQuery
+    const produceData = []
+    const meatData = []
+    const frozenData = []
+    const dairyData = []
+    const nutsData = []
+    const otherData = []
+
+    for (grocery of allData) {
+      switch (grocery.category) {
+        case "produce":
+          produceData.push(grocery)
+          break
+        case "meat":
+          meatData.push(grocery)
+          break
+        case "frozen":
+          frozenData.push(grocery)
+          break
+        case "dairy":
+          dairyData.push(grocery)
+          break
+        case "nutsAndSeeds":
+          nutsData.push(grocery)
+          break
+        default:
+          otherData.push(grocery)
+          break
+      }
+
+    }
     setStructuredData([
       {
         title: 'Produce',
@@ -31,6 +64,10 @@ const GroceryList = () => {
       {
         title: 'Nuts & Seeds',
         data: [...nutsData],
+      },
+      {
+        title: 'Other',
+        data: [...otherData],
       },
     ])
   }, [])
